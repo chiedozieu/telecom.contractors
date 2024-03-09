dc483e8d462f5c2153b76cd0e7361626f47f77a6
chiedozieu <chiedozieugwor@gmail.com>
2024-03-09 15:16:58 +0000

    added images, making dynamic pages

--- app.py
+++ app.py
@@ -2,9 +2,40 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'LAN Installation',
+    'location': 'Nigeria',
+    'cost': 'NGN 1,500,000' 
+  },
+  {
+    'id': 2,
+    'title': 'Fiber Installion',
+    'location': 'Nigeria',
+    'cost': 'NGN 70,000'
+  },
+    
+  
+  {
+    'id': 3,
+    'title': 'Radio Installation',
+    'location': 'Nigeria',
+    'cost': 'NGN 500,000'
+  
+  },
+  {
+   'id': 4,
+   'title': 'PABX Installation',
+   'location': 'Nigeria',
+   'cost': 'NGN 850,000'
+
+  }
+]
+ 
 @app.route('/')
 def hello_telecom():
-  return render_template('home.html')
+  return render_template('home.html', jobs = JOBS)
     
 
 
